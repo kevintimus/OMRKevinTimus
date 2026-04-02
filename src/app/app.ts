c88753e20d9e2eed62d045af21485793a50ee1cb
Kevin T <kevin.timus@institutvidreres.cat>
2026-04-02 22:51:15 +0200

    Acabado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,76 @@
-import { Component, signal } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Empleat } from './models/empleat.model';
+import { Doctor } from './models/doctor.model';
+import { ConnectorDb } from './services/connector-db';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+  constructor(private http: HttpClient, private connector: ConnectorDb, private cdr: ChangeDetectorRef) {}
+
+  doctors: Doctor[] = [];
+  numeroEmpleats = 0;
+  empleatABorrar = 0;
+  hospitals: any[] = [];
+
+  ngOnInit(): void {
+    /*
+    const EmpleatNou = new Empleat(67, 'Nose', 'QuePoner', 1234, new Date(), 23, 0, 10
+    );
+    this.http.post('http://localhost:3000/altaEmpleat', EmpleatNou).subscribe(res => console.log("Empelado creado ", res));
+     */
+
+    this.connector.llistaDoctors().subscribe({
+      next: res => {
+        console.log(res);
+        this.doctors = res;
+        this.cdr.detectChanges();
+      }
+    });
+
+    const departamentID = 20;
+
+    this.connector.empleatsDept(departamentID).subscribe({
+      next: res => {
+        console.log('Resultat ', res);
+        this.numeroEmpleats = res.numEmpleats;
+        this.cdr.detectChanges();
+      }
+    })
+
+    const doctorModificat = {
+      doctor_nom: 'KevinT',
+      doctor_especialitat: 'Examen Jordi',
+      doctor_hospital_codi: 22
+    }
+
+    this.connector.modificarDoctor(386, doctorModificat).subscribe({
+      next: res => console.log('Todo ha ido bien' + res),
+    })
+
+    this.connector.hospitalsInfo().subscribe({
+      next: res => {
+        console.log('Hospitales ', res);
+        this.hospitals = res;
+        this.cdr.detectChanges();
+      }
+    });
+
+    }
   protected readonly title = signal('OMRKevinTimus');
+
+  borrarEmpleat() {
+    this.connector.borrarEmpelado(this.empleatABorrar).subscribe({
+      next: res => console.log('Empleado eliminado:', res),
+    });
+  }
 }
--- src/app/server.js
+++ src/app/server.js
@@ -3,6 +3,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const { DataTypes } = require('sequelize');
 
 const app = express();
 const PORT = 3000;
@@ -13,11 +14,78 @@ app.use(express.json());
 const {crearConfigBaseDades} = require("./db.config.js")
 const db = crearConfigBaseDades() // conexion a la base de dades
 
+/*
 const {getModelCity} = require("./models/city.model.js")
 const City = getModelCity(db) // Model City de Sequalizer
 
 const {getModelCountry} = require("./models/country.model.js")
 const Country = getModelCountry(db) // Model Country de Sequalizer
+
+app.post('/CrearPais', async (req, res) => {
+  await City.create({city_id: 4, city: "Peru", country_id: 3, last_update: "2021-01-01" })
+})
+ */
+
+const Empleat = require("./models/empleat")(db, DataTypes);
+
+app.post('/altaEmpleat', async (req, res) => {
+  const empleat = await Empleat.create(req.body)
+  res.json(empleat)
+})
+
+const Doctor = require("./models/doctor")(db, DataTypes);
+
+app.get('/llistaDoctors', async (req, res) => {
+  const doctors = await Doctor.findAll({
+    where: { doctor_hospital_codi: 22 }
+  });
+  res.json(doctors)
+})
+
+app.get('/empleatsDept/:id', async (req, res) => {
+  const count = await Empleat.count({
+    where: { empleat_dept_num: req.params.id }
+  });
+  res.json({ departament: req.params.id, numEmpleats: count });
+})
+
+
+app.put('/ModificarDoctor/:codiDoctor', async (req, res) => {
+  await Doctor.update(req.body, {
+    where: { doctor_codi: req.params.codiDoctor }
+  })
+  res.json( "Doctor modificat");
+})
+
+app.delete('/borrarEmpleado/:numEmpleado', async (req, res) => {
+  await Empleat.destroy({
+    where: { empleat_num: req.params.numEmpleado }
+  });
+  res.json( "Empleado borrado");
+})
+
+
+const Hospital = require("./models/hospital")(db, DataTypes);
+const Sala = require("./models/sala")(db, DataTypes);
+
+Hospital.hasMany(Sala, { foreignKey: 'sala_hospital_codi' });
+Sala.belongsTo(Hospital, { foreignKey: 'sala_hospital_codi' });
+
+app.get('/hospitalsInfo', async (req, res) => {
+  const resultat = await Hospital.findAll({
+    attributes: [
+      'hospital_nom',
+      [db.fn('COUNT', db.col('salas.sala_codi')), 'numSales'],
+      [db.fn('SUM', db.col('salas.sala_nllits')), 'totalLlits']
+    ],
+    include: [{
+      model: Sala,
+      attributes: []
+    }],
+    group: ['hospital.hospital_codi']
+  });
+  res.json(resultat);
+});
 
 db.sync().then(() => {
   console.log("Drop and re-sync db")
